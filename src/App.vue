448ec36c9674fe3fc867b65d63c3ab623145d165
LS <1948275556@qq.com>
2021-12-21 11:10:23 +0800

    忽略掉package-lock.json

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
-  <div class="text-4xl text-center">测试应用</div>
+  <div class="text-4xl text-center">毕设工程</div>
   <el-container>
+
     <el-header class="bg-slate-300 p-12"></el-header>
 
-    <el-main><router-view /></el-main>
+    <el-main>
+      <router-view/>
+    </el-main>
 
     <el-footer class="bg-slate-300 p-12"></el-footer>
   </el-container>
+
 </template>
 
 <style>
@@ -14,7 +18,7 @@
 </style>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import {defineComponent} from 'vue'
 
 export default defineComponent({
   name: 'App',
