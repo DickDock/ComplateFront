b11e61c0fbf352703f32600c89cd5b5e1bc21212
LS <1948275556@qq.com>
2021-12-21 17:30:03 +0800

    添加Echarts图表实例 完善图标测试用例

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,10 +1,6 @@
 <template>
   <span class="text-4xl text-center block">Admin页面</span>
 
-  <div class="">
-    <DemoCharts1/>
-  </div>
-
   <el-card class="box-card mt-4 mx-auto" shadow="hover">
     <div class="text item">
       <el-button type="primary" @click="goIndex" class="bg-blue-400">返回首页</el-button>
@@ -19,10 +15,8 @@ import {defineComponent} from "vue";
 export default defineComponent({
   name: "Admin",
   components: {},
-  data(){
-    return {
-
-    }
+  data() {
+    return {}
   },
   methods: {
     goIndex() {
