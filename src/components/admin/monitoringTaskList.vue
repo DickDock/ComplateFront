d8d1293e42c01a9e104862a58b31fbb8f7011ff5
LS <1948275556@qq.com>
2021-12-27 17:30:59 +0800

    完善监控任务统计页面

--- src/components/admin/monitoringTaskList.vue
+++ src/components/admin/monitoringTaskList.vue
@@ -1,10 +1,20 @@
 <template>
-  <div class="">
-    <el-table :data="tableData" border style="width: 100%">
-      <el-table-column prop="date" label="Date" />
-      <el-table-column prop="name" label="Name" />
-      <el-table-column prop="address" label="Address" />
-    </el-table>
+  <div class="p-2 max-h-48 overflow-auto">
+    <div class="text-white border p-2">
+      <div class="grid grid-cols-5 gap-4">
+        <div class="col-span-2">任务名称</div>
+        <div class="">任务状态</div>
+        <div class="col-span-2">上次运行</div>
+      </div>
+
+      <div class="grid grid-cols-5 gap-4" v-for="data in tableData">
+        <div class="col-span-2">{{ data.name }}</div>
+        <div class="">
+          <el-switch v-model="data.status" active-color="#13ce66" inactive-color="#ff4949"/>
+        </div>
+        <div class="col-span-2">{{ data.lastDate }}</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -14,26 +24,20 @@ export default {
   data() {
     return {
       tableData: [
-        {
-          date: '2016-05-03',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-02',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-04',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-01',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
+        {lastDate: '2016-05-01', name: '测试任务', status: true},
+        {lastDate: '2016-05-02', name: '测试任务', status: true},
+        {lastDate: '2016-05-03', name: '测试任务', status: true},
+        {lastDate: '2016-05-04', name: '测试任务', status: false},
+        {lastDate: '2016-05-05', name: '测试任务', status: true},
+        {lastDate: '2016-05-06', name: '测试任务', status: false},
+        {lastDate: '2016-05-03', name: '测试任务', status: true},
+        {lastDate: '2016-05-03', name: '测试任务', status: true},
+        {lastDate: '2016-05-03', name: '测试任务', status: false},
+        {lastDate: '2016-05-03', name: '测试任务', status: true},
+        {lastDate: '2016-05-03', name: '测试任务', status: false},
+        {lastDate: '2016-05-03', name: '测试任务', status: true},
+        {lastDate: '2016-05-03', name: '测试任务', status: false},
+        {lastDate: '2016-05-03', name: '测试任务', status: true},
       ],
     }
   },
@@ -41,5 +45,12 @@ export default {
 </script>
 
 <style scoped>
+el-table, el-table tr, el-table td, el-table th {
+  background-color: transparent;
+  border: 0;
+}
+  /*overflow-x: hidden;*/
+/*overflow-y: hidden;*/
+::-webkit-scrollbar{width:0;}
 
 </style>
--- src/views/admin/DashBoard.vue
+++ src/views/admin/DashBoard.vue
@@ -3,12 +3,14 @@
     <div class="h-full w-full p-2">
       <span class="block text-4xl text-center text-white">系统概览</span>
       <div class="grid grid-cols-3 gap-4 mt-2">
-        <div class="p-4 bg-white rounded-3xl opacity-20">
-          <div class="">
-            <span class="text-xl text-fuchsia-800">监控任务统计</span>
-          </div>
-          <div class="">
-            <monitoring-task-list/>
+        <div class="p-4 bg-gray-50 rounded-3xl bg-opacity-20">
+          <div class="bg-transparent">
+            <div class="">
+              <span class="text-xl text-white">监控任务统计 >></span>
+            </div>
+            <div class="">
+              <monitoring-task-list/>
+            </div>
           </div>
         </div>
         <div class="p-24 bg-white h-56 rounded-3xl opacity-20"></div>
@@ -20,7 +22,6 @@
 
 <script>
 import monitoringTaskList from "../../components/admin/monitoringTaskList.vue";
-// import MonitoringTaskList from "../../components/admin/monitoringTaskList";
 
 export default {
   name: "DashBoard",
@@ -32,8 +33,10 @@ export default {
 
 <style scoped>
 #mainBox {
-  background-image: url('../../assets/images/dashbg.jpg');
+  /*background-image: url('../../assets/images/dashbg.jpg');*/
   width: 100%;
   height: 100%;
+  background-color: #21D4FD;
+  background-image: linear-gradient(19deg, #21D4FD 0%, #B721FF 100%);
 }
 </style>
