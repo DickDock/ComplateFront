0331e02d0bf7975692d729e044b3861a33b56ee6
LS <1948275556@qq.com>
2022-02-10 13:22:26 +0800

    重写主面板

--- src/views/admin/DashBoard.vue
+++ src/views/admin/DashBoard.vue
@@ -3,8 +3,16 @@
     <div class="titleDiv">
       <span class="">主机安全态势预警平台</span>
     </div>
-    <div class="grid grid-cols-3 grid-rows-3 gap-4 mt-2">
-      <div class="bg-blue-400 p-36" v-for="n in 9"></div>
+    <div class="grid grid-cols-3 grid-rows-3 gap-4 mt-6">
+      <div class="topLeft">
+        <div class="">
+          <span class="title">主机资源概览</span>
+        </div>
+      </div>
+      <div class="bg-blue-400 p-36 row-span-3"></div>
+      <div class="bg-blue-400 p-36"></div>
+      <div class="bg-blue-400 p-36 row-span-2"></div>
+      <div class="bg-blue-400 p-36"></div>
     </div>
   </div>
 </template>
@@ -21,13 +29,23 @@ export default defineComponent({
 </script>
 
 <style scoped>
-.mainBox{
+.mainBox {
   background-image: url('@/assets/images/dashboard/mainBg.png');
 }
+
 .titleDiv {
-  @apply bg-cover
+  @apply bg-cover;
 }
+
 .titleDiv span {
-  @apply text-center text-4xl block text-white mt-2
+  @apply text-center text-4xl block text-white mt-2;
+}
+
+.topLeft {
+  @apply row-span-2 p-2 bg-cover;
+  background-image: url("@/assets/images/dashboard/topLeft.png");
+}
+.topLeft span.title {
+  @apply bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-2xl block;
 }
 </style>
