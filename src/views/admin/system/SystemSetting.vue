1630d13395c00a7ecbb4f689f6a4cb86e30c4a50
LS <1948275556@qq.com>
2022-03-04 18:12:36 +0800

    编写系统信息页面

--- src/views/admin/system/SystemSetting.vue
+++ src/views/admin/system/SystemSetting.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="w-full h-full">
     <div class="grid grid-cols-2 gap-4 w-full h-full">
-      <div class="w-full h-full bg-amber-300">
+      <div class="w-full h-full">
+        <p class="text-2xl mb-2">CPU信息</p>
+        <el-card class="box-card">
+          <div v-for="o in 4" :key="o" class="text item">{{ 'List item ' + o }}</div>
+        </el-card>
       </div>
-      <div class="w-full h-full bg-lime-300"></div>
+      <div class="w-full h-full">
+        <p class="text-2xl mb-2">内存信息</p>
+        <el-card class="box-card">
+          <div v-for="o in 4" :key="o" class="text item">{{ 'List item ' + o }}</div>
+        </el-card>
+      </div>
     </div>
   </div>
 </template>
