ce5c9935f3255452dbee145bb6136aa8293e99b5
LS <1948275556@qq.com>
2022-03-08 17:53:00 +0800

    更新依赖版本

--- auto-imports.d.ts
+++ auto-imports.d.ts
@@ -2,6 +2,5 @@
 // We suggest you to commit this file into source control
 declare global {
   const ElMessage: typeof import('element-plus/es')['ElMessage']
-  const ElMessageBox: typeof import('element-plus/es')['ElMessageBox']
 }
 export {}
